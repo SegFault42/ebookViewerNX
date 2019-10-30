72e0c28adda925cb5f7271a25dad5770c1a3a0c4
SegFault42 <SegFault42@protonmail.com>
2019-10-30 21:55:55 +0100

    center page in the middle of screen

--- include/graphic.h
+++ include/graphic.h
@@ -1,11 +1,14 @@
 #ifndef GRAPHIC_H
 #define GRAPHIC_H
 
-typedef struct	s_graphic
+#define WIN_WIDTH	1280
+#define WIN_HEIGHT	720
+
+typedef struct		s_graphic
 {
-	SDL_Window	*win;
-	SDL_Renderer *renderer;
-}				t_graphic;
+	SDL_Window		*win;
+	SDL_Renderer	*renderer;
+}					t_graphic;
 
 t_graphic	*init(void);
 void		deinit(void);
