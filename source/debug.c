e48e4cc1caebdd6290bff6abdfbe486835eebe39
SegFault42 <SegFault42@protonmail.com>
2019-12-12 13:58:57 +0100

    fix debug hitbox

--- source/debug.c
+++ source/debug.c
@@ -9,19 +9,24 @@ static void	draw_hitbox(SDL_Rect rect)
 	SDL_SetRenderDrawColor(graphic->renderer, 255, 0, 0, 100);
 	SDL_RenderFillRect(graphic->renderer, &rect);
 
-	SDL_SetRenderDrawColor(graphic->renderer, 255, 255, 0, 100);
+	SDL_SetRenderDrawColor(graphic->renderer, 255, 255, 0, 255);
 	SDL_RenderDrawRect(graphic->renderer, &rect);
 }
 
 void	debug_draw_hitbox(void)
 {
-	draw_hitbox(layout->help_home);
-	draw_hitbox(layout->exit_home);
+
 	draw_hitbox(layout->next_page_button);
 	draw_hitbox(layout->prev_page_button);
-	draw_hitbox(layout->rotate_button);
+	draw_hitbox(layout->bar.back_bar);
 
-	if (ebook->read_mode == false) {
-		draw_hitbox(layout->cover);
+	if (layout->show_bar == true) {
+		draw_hitbox(layout->help_home);
+		draw_hitbox(layout->exit_home);
+		if (ebook->read_mode == true) {
+			draw_hitbox(layout->rotate_button);
+		} else {
+			draw_hitbox(layout->cover);
+		}
 	}
 }
